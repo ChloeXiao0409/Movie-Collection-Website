0b5a2217ffff9e0f6fce826c66587e3b2384fd40
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-03-04 11:12:17 +1100

    Update-Server-Init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res, next) => (
+    res.json("Hello World")
+));
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
