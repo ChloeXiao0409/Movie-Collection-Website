d6b262fffe5977e7d77c7b92663706e8b8b8bc61
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-03-04 14:21:40 +1100

    Mission

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 
 // Debug Mode
 
+const movies = [
+    {
+      id: 1,
+      title: "Inception",
+      description: "A skilled thief steals secrets from dreams.",
+      types: ["Sci-Fi"],
+      averageRating: 4.5,
+      reviews: [
+        { id: 1, content: "Amazing movie!", rating: 5 },
+        { id: 2, content: "Great visuals.", rating: 4 },
+      ],
+    },
+];
+
+
 app.get("/", (req, res, next) => (
     //breakpoint check
     console.log(req),
