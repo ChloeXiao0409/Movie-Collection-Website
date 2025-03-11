f1fe5207dc16634eb17c24bce965916033ecbfdc
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-03-12 10:18:50 +1100

    Update-helmet-Secruity

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 // Read environment variables need to be at the top of the file
-// require("dotenv").config({
-//   path: `.env.${process.env.NODE_ENV || "development"}`,
-// });
-require('dotenv').config();
 const config = require('./utils/config');
 const cors = require('cors');
+// For Cyber Security
+const helmet = require("helmet");
 const express = require('express');
 const v1Router = require('./routes');
-// const { config } = require('dotenv');
 
 const app = express();
-// const corsMiddleware = require('./middleware/cors.middleware');
 
-// Add the middleware to parse the body of the request
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-// app.use(corsMiddleware); 
 
 // Router
 app.use("/v1", v1Router);
