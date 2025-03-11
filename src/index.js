7f87cb1d08073ee532e7157d6135fdb8d1400f7b
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-03-11 18:01:55 +1100

    Update-cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const movieRouter = require('./routes/movie.router');
-const app = express();
 const cors = require('cors');
-const corsMiddleware = require('./middleware/cors.middleware');
+const express = require('express');
 const v1Router = require('./routes');
+
+const app = express();
+// const corsMiddleware = require('./middleware/cors.middleware');
+
 // Add the middleware to parse the body of the request
+app.use(cors());
 app.use(express.json());
-app.use(corsMiddleware); // Enable CORS for all routes
+// app.use(corsMiddleware); 
 
 // Router
 app.use("/v1", v1Router);
 
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-  });
+  console.log("Server is listening on port 3000");
+});
