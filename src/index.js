edc926aa3a720326f2a3bce58664d2debd448460
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-03-12 10:25:09 +1100

    Update-morgan-recording-log

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ const config = require('./utils/config');
 const cors = require('cors');
 // For Cyber Security
 const helmet = require("helmet");
+// Morgan is for recording who is accessing the server, show the IP address, the method, the URL, the status code, the response time, and the size of the response
+const morgan = require("morgan");
 const express = require('express');
 const v1Router = require('./routes');
+
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
+app.use(morgan(config.NODE_ENV === "development" ? "dev" : "combined"));
 app.use(express.json());
 
 // Router
